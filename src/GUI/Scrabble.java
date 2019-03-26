c18196e9c1615d471216d7cf7eb571a6b48761fe
paola0298 <pvillegasch@gmail.com>
2019-03-25 21:54:21 -0600

    Agregación de pantalla de juego

--- src/GUI/Scrabble.java
+++ src/GUI/Scrabble.java
@@ -9,9 +9,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Scrabble extends Application {
     private String cwd = System.getProperty("user.dir");
@@ -43,9 +48,83 @@ public class Scrabble extends Application {
         });
         joinMatchContainer.getChildren().addAll(joinTitle, joinTextFiel, joinButton, joinResponse);
 
+        /////////////////////////////Pantalla de Juego//////////////////////
+
+        BorderPane gameScreenContainer = new BorderPane();
+
+        //Imagen del usuario
+        Image userImage = imageLoader(cwd + "/res/userIcon.png");
+        ImageView addUserImage = new ImageView(userImage);
+        addUserImage.setFitHeight(120);
+        addUserImage.setFitWidth(120);
+
+        //puntuacion del usuario
+        HBox userScoreBox = new HBox();
+        userScoreBox.setSpacing(10);
+        userScoreBox.setAlignment(Pos.CENTER);
+
+
+        Text userScoreText = new Text("Mi puntuación es: ");
+        Text userScore = new Text("50");
+
+        userScoreBox.getChildren().addAll(userScoreText, userScore);
+
+        //Fichas
+
+        Image token = imageLoader(cwd + "/res/token.png");
+        ImageView tokenImage = new ImageView(token);
+        tokenImage.setFitWidth(501);
+        tokenImage.setFitHeight(82);
+
+
+        //Boton para enviar la palabra
+        Image scrabbleImage = imageLoader(cwd + "/res/scrabble.jpg");
+        ImageView scrabbleImageButton = new ImageView(scrabbleImage);
+        scrabbleImageButton.setFitWidth(150);
+        scrabbleImageButton.setFitHeight(150);
+        scrabbleImageButton.setOnMouseClicked(mouseEvent -> {
+            //tomar palabra creada y enviarla al servidor
+            System.out.println("The word is: ");
+        });
+
+        // contenedores
+
+        VBox scoreAndToken = new VBox();
+        scoreAndToken.setAlignment(Pos.CENTER);
+        scoreAndToken.setSpacing(10);
+        scoreAndToken.getChildren().addAll(userScoreBox, tokenImage); //agregar las fichas
+
+        HBox actualPlayerInfoContainer = new HBox();
+        actualPlayerInfoContainer.setStyle("-fx-background-color: white");
+        actualPlayerInfoContainer.setAlignment(Pos.CENTER);
+        actualPlayerInfoContainer.setSpacing(15);
+        actualPlayerInfoContainer.setPrefHeight(210);
+        actualPlayerInfoContainer.getChildren().addAll(addUserImage, scoreAndToken, scrabbleImageButton);
+
+        VBox upPlayerInfoContainer = new VBox();
+        upPlayerInfoContainer.setStyle("-fx-background-color: green");
+        upPlayerInfoContainer.setPrefHeight(150);
+
+        GridPane matrixContainer = new GridPane();
+        matrixContainer.setStyle("-fx-background-color: purple");
+
+        VBox rightPlayerInfoContainer = new VBox();
+        rightPlayerInfoContainer.setStyle("-fx-background-color: red");
+        rightPlayerInfoContainer.setPrefWidth(170);
+
+        VBox leftPlayerInfoContainer = new VBox();
+        leftPlayerInfoContainer.setStyle("-fx-background-color: orange");
+        leftPlayerInfoContainer.setPrefWidth(170);
+
+        gameScreenContainer.setTop(upPlayerInfoContainer);
+        gameScreenContainer.setBottom(actualPlayerInfoContainer);
+        gameScreenContainer.setCenter(matrixContainer);
+        gameScreenContainer.setRight(rightPlayerInfoContainer);
+        gameScreenContainer.setLeft(leftPlayerInfoContainer);
+
 
         //Aquí añaden su panel al contenedor principal.
-        mainLayout.getChildren().addAll(joinMatchContainer);
+        mainLayout.getChildren().addAll(joinMatchContainer, gameScreenContainer);
         Scene scene = new Scene(mainLayout, 1280, 720);
         scene.getStylesheets().add("file:///" + cwd + "/res/styles.css");
         stage.setMinWidth(640);
@@ -57,6 +136,19 @@ public class Scrabble extends Application {
 
     public void show() {
         launch(Scrabble.class);
+    }
+
+
+    private Image imageLoader(String path){
+        try{
+            FileInputStream i = new FileInputStream(path);
+            Image img = new Image(i);
+            return img;
+        }catch (FileNotFoundException e){
+            System.out.println("Couldn't load images!");
+        }
+        System.out.println("Returning null");
+        return null;
     }
 
 }
