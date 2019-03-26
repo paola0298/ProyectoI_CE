92173677c9a1bff5d78fa42078cd54fd0658e019
Marlon-vc <mvegach2013@gmail.com>
2019-03-25 22:15:46 -0600

    Creación de métodos de interacción con el servidor

--- src/GUI/Scrabble.java
+++ src/GUI/Scrabble.java
@@ -69,7 +69,6 @@ public class Scrabble extends Application {
 
         //Pantalla de unión a partida existente.
         VBox joinMatchContainer = new VBox();
-        joinMatchContainer.setStyle("-fx-background-color: gray;");
         joinMatchContainer.setAlignment(Pos.CENTER);
         joinMatchContainer.setSpacing(15);
         joinMatchContainer.setPadding(new Insets(15));
@@ -82,7 +81,6 @@ public class Scrabble extends Application {
         joinResponse.setId("join_message");
         joinButton.setOnAction(event -> {
             //Decirle a controller que envie un request al servidor.
-            controller.doAction(ACTIONS.CREATE_MATCH);
         });
         joinMatchContainer.getChildren().addAll(joinTitle, joinTextField, joinButton, joinResponse);
 
