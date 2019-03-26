9f55994e77aac1d425040a2fdc4989fb551501dc
Brayan-00 <brayanfernando.10@gmail.com>
2019-03-25 23:42:59 -0600

    Agrego la clase Player y un adelanto de la pantalla inicial del juego

--- src/GUI/Scrabble.java
+++ src/GUI/Scrabble.java
@@ -28,6 +28,34 @@ public class Scrabble extends Application {
 
         // En este panel va a meter sus paneles con su parte de la interfaz.
         StackPane mainLayout = new StackPane();
+        /////////////////////////////Pantalla de Inicio//////////////////////
+        /* Pantalla inicial donde el jugador puede elegir entre ingresar a un partida, o crear una propia
+        Cuando se crea una parida nueva se genera un código con el que se puede invitar a otros jugadores
+        En esta ventana se especifíca cuantos jugadores tendrá la partida
+         */
+        VBox initialWindow = new VBox();
+        initialWindow.setStyle("-fx-background-color: #1a8c24;");
+        initialWindow.setAlignment(Pos.TOP_LEFT);
+        initialWindow.setSpacing(15);
+        initialWindow.setPadding(new Insets(15));
+        // El usuario ingresa su nombre
+        Label Players_Name = new Label("Introduzca su nombre:");
+        TextField Players_Name_Input = new TextField();
+        Players_Name_Input.setMaxWidth(300);
+
+        // El usuario escoge si quiere unirse a una partida o si va a crear una nueva
+        Button Join = new Button("Unirme a una partida existente");
+        Button New_Game = new Button("Crear una nueva partida");
+        Join.setTranslateY(30);
+        New_Game.setTranslateY(50);
+
+
+        initialWindow.getChildren().addAll(Players_Name,Players_Name_Input,Join,New_Game);
+
+
+
+
+
 
         //Pantalla de unión a partida existente.
         VBox joinMatchContainer = new VBox();
@@ -37,8 +65,8 @@ public class Scrabble extends Application {
         joinMatchContainer.setPadding(new Insets(15));
         Label joinTitle = new Label("Ingresa el código de la partida");
         joinTitle.setId("");
-        TextField joinTextFiel = new TextField();
-        joinTextFiel.setMaxWidth(200);
+        TextField joinTextField = new TextField(); //Brayan: Agregué una d al final del nombre de la variable, en la siguiente línea también
+        joinTextField.setMaxWidth(200);
         Button joinButton = new Button("Unirse");
         Label joinResponse = new Label("");
         joinResponse.setId("join_message");
@@ -46,9 +74,10 @@ public class Scrabble extends Application {
             //Decirle a controller que envie un request al servidor.
             controller.doAction(ACTIONS.CREATE_MATCH);
         });
-        joinMatchContainer.getChildren().addAll(joinTitle, joinTextFiel, joinButton, joinResponse);
+        joinMatchContainer.getChildren().addAll(joinTitle, joinTextField, joinButton, joinResponse);
 
         /////////////////////////////Pantalla de Juego//////////////////////
+
 
         BorderPane gameScreenContainer = new BorderPane();
 
@@ -123,8 +152,9 @@ public class Scrabble extends Application {
         gameScreenContainer.setLeft(leftPlayerInfoContainer);
 
 
+
         //Aquí añaden su panel al contenedor principal.
-        mainLayout.getChildren().addAll(joinMatchContainer, gameScreenContainer);
+        mainLayout.getChildren().addAll(joinMatchContainer, gameScreenContainer, initialWindow);
         Scene scene = new Scene(mainLayout, 1280, 720);
         scene.getStylesheets().add("file:///" + cwd + "/res/styles.css");
         stage.setMinWidth(640);
--- src/Logic/Player.java
+++ src/Logic/Player.java
@@ -1,4 +1,15 @@
 package Logic;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 public class Player {
+    String Player_ID;
+    String Name;
+    int Player_Number;
+
+    public void create_ID (){
+        this.Player_ID = RandomStringUtils.randomAlphanumeric(6);
+
+    }
+
 }
