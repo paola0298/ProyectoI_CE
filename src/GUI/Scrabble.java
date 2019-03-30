c5c33fdcab2d7d0a7b9df5ef1f4afeddb33ed2c6
Marlon-vc <mvegach2013@gmail.com>
2019-03-29 20:22:01 -0600

    .

--- src/GUI/Scrabble.java
+++ src/GUI/Scrabble.java
@@ -4,8 +4,6 @@ import Logic.Controller;
 import Structures.LinkedList;
 import Structures.Node;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
