79824a668e9e77dd3a47ca1e566e84b84b9ef345
Brayan-00 <brayanfernando.10@gmail.com>
2019-04-02 20:11:44 -0600

    Se hacen ligeros cambios, se agrega la clase token

--- src/Logic/Player.java
+++ src/Logic/Player.java
@@ -1,18 +1,27 @@
 package Logic;
 
-//TODO actualizar la librería a la última versión
-//import org.apache.commons.lang.RandomStringUtils;
 
+import Structures.LinkedList;
 import org.apache.commons.lang3.RandomStringUtils;
 
 
 public class Player {
     String Player_ID;
     String Name;
+    LinkedList<Token> tokenlist;
 
+    public Player(String name) {
+        create_ID();
+        Name = name;
+    }
 
     public void create_ID (){
         this.Player_ID = RandomStringUtils.randomAlphanumeric(6);
+
+    }
+    public void create_tokens(){
+
+
 
     }
 
