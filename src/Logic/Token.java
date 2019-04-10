24da6ea8e2a2d20fc054541659e207cc299cd303
paola0298 <pvillegasch@gmail.com>
2019-04-09 20:45:56 -0600

    Creación de constructor por defecto, getters y setters

--- src/Logic/Token.java
+++ src/Logic/Token.java
@@ -5,18 +5,33 @@ public class Token {
     private int score;
     private String letter;
 
+    public Token(){}
+
     public Token(String imagePath, int score, String letter) {
         this.imagePath = imagePath;
         this.score = score;
         this.letter = letter;
     }
 
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
     public String getImagePath() {
         return imagePath;
     }
 
+    public void setScore(int score) {
+        this.score = score;
+    }
+
     public int getScore() {
         return score;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+
     }
 
     public String getLetter() {
