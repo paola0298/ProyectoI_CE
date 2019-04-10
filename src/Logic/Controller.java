4c37d710c0d0a438cd29316275eea6080f320a6b
paola0298 <pvillegasch@gmail.com>
2019-04-09 20:42:38 -0600

    Método para crear una nueva partida

--- src/Logic/Controller.java
+++ src/Logic/Controller.java
@@ -19,61 +19,92 @@ public class Controller {
     private String playerName = "-";
     private String player_id = "-";
     private String current_match_id = "-";
-
     private Game actualGame;
-
-    public Controller(Scrabble gui) {
-        this.gui = gui;
-        initialize();
-    }
+    private Player playerInstance;
 
 
     /**
-     * Este método obtiene el objeto Game de la partida actual
+     * Este método es el constructor de la clase
+     * @param gui La interfaz que va a controlar la clase
      */
-    private Game getActualGame(){
-        ObjectMapper objectMapper = new ObjectMapper();
-        message = prepare();
-        message.put("action", "GET_GAME");
-        message.put("match_id", current_match_id);
-
-        response = client.connect(message);
-
-        if (response.get("status").equals("SUCCESS")) {
-            // devolver el objeto deserializado
-            String stringGame = response.getString("object_game");
-            try {
-                actualGame = objectMapper.readValue(stringGame, Game.class);
-            } catch (IOException e) {
-                System.out.println("Error getting game object");
-            }
-
-        } else {
-            System.out.println("Could not create match");
-        }
-        return new Game(4);
+    public Controller(Scrabble gui) {
+        this.gui = gui;
+        initialize1();
     }
 
-
-    public void updateGrid(){
-
+    /**
+     * @return Devuelve la instancia de la partida actual
+     */
+    public Game getActualGame() {
+        return actualGame;
     }
 
+    /**
+     * @return Devuelve el identificador de la partida actual
+     */
+    public String getCurrent_match_id() {
+        return current_match_id;
+    }
+
+    /**
+     * Este método coloca la instancia de la partica actual
+     * @param actualGame Instancia de la partida actual
+     */
+    public void setActualGame(Game actualGame) {
+        this.actualGame = actualGame;
+    }
+
+    /**
+     * @return La instancia del jugador actual
+     */
+    public Player getPlayerInstance() {
+        return playerInstance;
+    }
+
+    /**
+     * Este método coloca la instancia del jugador actual
+     * @param playerInstance Instancia del jugador actual
+     */
+    public void setPlayerInstance(Player playerInstance) {
+        this.playerInstance = playerInstance;
+    }
+
+    /**
+     * Este método le coloca el nombre al jugador
+     * @param playerName Parámetro del nombre del jugador
+     */
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
 
     /**
      * Este método le pide al servidor crear una nueva partida con los jugadores máximos especificados
      * @param max_players Cantidad máxima de jugadores en la partida.
      */
-    public void create_match(int max_players) {
+    public void create_match(String max_players) {
         message = prepare();
         message.put("action", "CREATE_MATCH");
         message.put("max_players", max_players);
+        message.put("player_name", playerName);
 
         response = client.connect(message);
 
         if (response.get("status").equals("SUCCESS")) {
             System.out.println("Match created successfully");
+            ObjectMapper objectMapper =  new ObjectMapper();
             player_id = response.getString("player_id");
+            current_match_id = response.getString("game_id");
+            String stringGame = response.getString("game");
+            String stringPlayer = response.getString("player");
+
+            try {
+                actualGame = objectMapper.readValue(stringGame, Game.class);
+                playerInstance = objectMapper.readValue(stringPlayer, Player.class);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error getting game object");
+            }
 
         } else {
             System.out.println("Could not create match");
@@ -148,10 +179,6 @@ public class Controller {
 
     }
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
     /**
      * Este método prepara un nuevo objeto JSON para usar en los pedidos al servidor.
      * @return Objeto JSON base para generar cada pedido al servidor.
@@ -178,5 +205,11 @@ public class Controller {
             System.out.println("[Error] Could not read settings");
         }
     }
+
+    private void initialize1(){
+        client = new Client("localhost", 6307);
+    }
+
+
 
 }
